e9f29c49f5a4544b60759895ad24a33b580c31ca
Paul Arizpe Colorado <kiramishima@outlook.com>
2019-09-10 16:09:00 -0500

    update repo

---
+++ Library.fs
@@ -0,0 +1,22 @@
+﻿namespace HelloFunctions
+
+open Microsoft.Azure.WebJobs
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+// open Microsoft.Azure.WebJobs.Host
+open Microsoft.Extensions.Logging
+open System.IO
+open System.Text
+
+module Function1 =
+    let Say name =
+        printfn "Hello %s" name
+
+    [<FunctionName("HelloFunction")>]
+    let Run([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)>] req : HttpRequest, log: ILogger) =
+        log.LogInformation("F# HTTP trigger function processed a request.")
+        OkObjectResult "Nino Nino"
+
+
+
