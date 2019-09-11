477dcfd7427e802a9afd2b06eb7a6d3f7eb4284f
Paul Arizpe Colorado <kiramishima@outlook.com>
2019-09-11 17:18:58 -0500

    Archivos finales del ejemplo usado para Medium

--- Library.fs
+++ Library.fs
@@ -4,17 +4,20 @@ open Microsoft.Azure.WebJobs
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
-// open Microsoft.Azure.WebJobs.Host
 open Microsoft.Extensions.Logging
 open System.IO
 open System.Text
 
-module Function1 =
+module HelloFunction =
     let Say name =
         printfn "Hello %s" name
 
+    // Nombre de la funcion
     [<FunctionName("HelloFunction")>]
-    let Run([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)>] req : HttpRequest, log: ILogger) =
+    // Nivel de Autorizacion para consumir la funcion, la cambiamos a Anonymous ya que para este ejemplo no requerimos nada de seguridad
+    // Tambien que metodos HTTP acepta
+    // Route nos permite customizar el nombre de salida al publicar la funcion
+    let Run([<HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)>] req : HttpRequest, log: ILogger) =
         log.LogInformation("F# HTTP trigger function processed a request.")
         OkObjectResult "This is an example of Azure Function with F#"
 
